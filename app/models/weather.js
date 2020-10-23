b6e5becbb7ccb38802f324a1cc9c5d829a6130c5
Michelle Charron <michelle.charron93@gmail.com>
2020-10-23 15:55:15 -0600

    CO logs for weather Api

--- app/models/weather.js
+++ app/models/weather.js
@@ -1,13 +1,10 @@
 export default class Weather {
   constructor(data) {
-    console.log('[RAW WEATHER API DATA]', data);
+    // console.log('[RAW WEATHER API DATA]', data);
     //NOTE Have you ever wanted to know the temperature measured in kelvin? 
     //      That is what this data returns! data.main.temp is the temperature in Kelvin
-
-
     //TODO You should convert the temperature data to either F or C
     //      check out the other data that comes back and see if there is anything you want to try
-
     this.city = data.name
     this.kelvin = data.main.temp
     this.visibility = data.weather[0].main
@@ -22,7 +19,7 @@ export default class Weather {
   get Template() {
     return /*html */`
     <h4>${this.city}</h4>
-    <h4>${this.kelvin}</h4>
+    <h4>${this.kelvin} F</h4>
     <h4>${this.visibility}</h4>
 
     `
