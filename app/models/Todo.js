8c914b34cff657ca61aba24e58108be1db0fcaab
Michelle Charron <michelle.charron93@gmail.com>
2020-10-24 14:32:10 -0600

    Style edits

--- app/models/Todo.js
+++ app/models/Todo.js
@@ -11,14 +11,14 @@ export default class Todo {
     <h3>${this.description}</h3>
     <div>
     ${this.ToggledButton}
-    <button class="btn btn-warning btn-sm" onclick="app.todoController.removeTodo('${this.id}')">&times</button></div>
+    <button class="btn btn-danger btn-sm" onclick="app.todoController.removeTodo('${this.id}')">&times</button></div>
 </div>
     `
   }
 
   get ToggledButton() {
     let checkButton = `</div><button class="btn btn-success btn-sm" onclick="app.todoController.toggleTodoStatus('${this.id}')">&#10003</button>`
-    let doneButton = `<button class="btn btn-warning btn-sm" onclick="app.todoController.toggleTodoStatus('${this.id}')">Undo</button>`
+    let doneButton = `<button class="btn btn-warning btn-sm" onclick="app.todoController.toggleTodoStatus('${this.id}')"><i class="fas fa-undo"></i></button>`
     if (this.completed) {
       return doneButton
     } else {
