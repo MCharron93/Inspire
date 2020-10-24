373459438ad86d15b34280851b855ce91dba47b5
Michelle Charron <michelle.charron93@gmail.com>
2020-10-24 15:05:51 -0600

    Strikethrough for toggled text added

--- app/models/Todo.js
+++ app/models/Todo.js
@@ -8,7 +8,7 @@ export default class Todo {
   get Template() {
     return /*html*/`
     <div class="col-4 p-2">
-    <h3>${this.description}</h3>
+    ${this.ToggledText}
     <div>
     ${this.ToggledButton}
     <button class="btn btn-danger btn-sm" onclick="app.todoController.removeTodo('${this.id}')">&times</button></div>
@@ -16,10 +16,20 @@ export default class Todo {
     `
   }
 
+  get ToggledText() {
+    let description = `<h3>${this.description}</h3>`
+    let striked = `<h3><s>${this.description}</s></h3>`
+    if (this.completed) {
+      return striked
+    } else {
+      return description
+    }
+  }
   get ToggledButton() {
     let checkButton = `</div><button class="btn btn-success btn-sm" onclick="app.todoController.toggleTodoStatus('${this.id}')">&#10003</button>`
     let doneButton = `<button class="btn btn-warning btn-sm" onclick="app.todoController.toggleTodoStatus('${this.id}')"><i class="fas fa-undo"></i></button>`
     if (this.completed) {
+
       return doneButton
     } else {
       return checkButton
